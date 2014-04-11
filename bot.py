83a1e402413039555076b23909dec511ef78a915
zerosalife <zerosalife@gmail.com>
2014-04-10 21:01:19 -0500

    bot works.  some pep8 cleanup as well.

--- bot.py
+++ bot.py
@@ -1,32 +1,41 @@
 #!/usr/bin/python
 import init_twit as tw
-import markovgen, time, re, random 
+import markovgen
+import time
+import re
+import random
+import os
 
 # absolute path to corpus file
-corpus_file = '/home/jk/Code/bots/lovecraft_ebooks/corpus.txt'
+corpus_file = '.' + os.path.dirname(__file__) + os.sep + 'corpus.txt'
+
 with open(corpus_file) as text:
     markov = markovgen.Markov(text)
 
+
 def genTweet():
-    wc = random.randint(6,18)
+    wc = random.randint(6, 18)
     return markov.generate_markov_text(size=wc)
 
-def tweet(status,irtsi=None,at=None): 
+
+def tweet(status, irtsi=None, at=None):
     try:
         if at and irtsi:
-            status = "@"+at+" "+status
-            tw.poster.statuses.update(status=status,in_reply_to_status_id=irtsi)
+            status = "@" + at + " " + status
+            tw.poster.statuses.update(status=status,
+                                      in_reply_to_status_id=irtsi)
         else:
-            #print status    
+            #print status
             tw.poster.statuses.update(status=status)
     except tw.TwitterError:
         print "Twitter Error"
     else:
-        if irtsi: 
-            status = "In reply to "+irtsi+": "+status
+        if irtsi:
+            status = "In reply to " + irtsi + ": " + status
         print status
 
-def reply(txt,mention):
+
+def reply(txt, mention):
     asker = mention['from_user']
     print asker + " said " + mention['text']
     status_id = str(mention['id'])
@@ -34,16 +43,24 @@ def reply(txt,mention):
         tw.last_id_replied = status_id
     while len(txt) > 123:
         txt = genTweet()
-    tweet(txt,status_id,asker)
+    tweet(txt, status_id, asker)
 
 results = []
-results = tw.twitter.search(q="@"+tw.handle,since_id=tw.last_id_replied)['results']
-retweets = re.compile('rt\s',flags=re.I)
-results = [response for response in results if not retweets.search(response['text'])]
+try:
+    results = tw.twitter.search(q="@" + tw.handle,
+                                since_id=tw.last_id_replied)['results']
+
+    retweets = re.compile('rt\s', flags=re.I)
+
+    results = [response for response in results
+               if not retweets.search(response['text'])]
+except:
+    results = []
+
 if not results:
     print "Nobody's talking to me..."
 else:
-    [reply(genTweet(),result) for result in results] 
+    [reply(genTweet(), result) for result in results]
 tweet(genTweet())
-    print "Sweet Dreams..."
-    time.sleep(7600) # waits for two hours
+print "Sweet Dreams..."
+time.sleep(7600)                        # waits for two hours
--- init_twit.py
+++ init_twit.py
@@ -2,27 +2,41 @@ from twitter import Twitter, TwitterError
 from twitter.oauth import OAuth, read_token_file
 from twitter.oauth_dance import oauth_dance
 import os
+import init_cred
 
 # get your own at https://dev.twitter.com/apps/new
-CONSUMER_KEY='MSkGnYHDUgDxZbTBDkfP8Q'
-CONSUMER_SECRET='BEN3D56OWddggyuYUUcENWK1OPmqVCy5EGIq1mg'
+CONSUMER_KEY = 'MSkGnYHDUgDxZbTBDkfP8Q'
+CONSUMER_SECRET = 'BEN3D56OWddggyuYUUcENWK1OPmqVCy5EGIq1mg'
+CONSUMER_KEY = init_cred.API_key
+CONSUMER_SECRET = init_cred.API_secret
 
-# path to where your oauth credentials are stored; by default .oauth in the same directory
+# path to where your oauth credentials are stored; by default .oauth
+# in the same directory
 oauth_filename = os.path.dirname(__file__) + os.sep + '.oauth'
-# use the function 'from twitter.oauth import write_token_file' to create this file
+# use the function 'from twitter import oauth_dance' to create this
+# file
 oauth_token, oauth_token_secret = read_token_file(oauth_filename)
 
 twitter = Twitter(domain='search.twitter.com')
 twitter.uriparts = ()
 
 poster = Twitter(
-	auth=OAuth(
-		oauth_token, oauth_token_secret, CONSUMER_KEY, CONSUMER_SECRET),
-		secure=True,
-		api_version='1.1',
-		domain='api.twitter.com')
+    auth=OAuth(oauth_token,
+               oauth_token_secret,
+               CONSUMER_KEY,
+               CONSUMER_SECRET),
+    secure=True,
+    api_version='1.1',
+    domain='api.twitter.com')
 
 # get the status_id of the last tweet to which you replied
-last_id_replied = [tweet['in_reply_to_status_id'] for tweet in poster.statuses.user_timeline() if tweet['in_reply_to_status_id'] != None][0]
+try:
+    last_id_replied = [tweet['in_reply_to_status_id']
+                       for tweet in poster.statuses.user_timeline()
+                       if tweet['in_reply_to_status_id'] != None][0]
+except:
+    last_id_replied = None
+
+
 # your bot's twitter handle
-handle = "lovecraft_ebook"
+handle = "styx_ebooks"
